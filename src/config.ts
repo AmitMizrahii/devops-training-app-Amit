530a6c29b926ced3a457a4ff014b1e2a419895bf
miamit112@gmail.com <miamit112@gmail.com>
2024-06-23 11:51:51 +0300

    asd

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,22 @@
-export const PG_URL = process.env.PG_URL || 'postgres://postgres:password@db:5432/node_app';
+export const PG_URL = process.env.PG_URL;
 export const PORT = Number(process.env.PORT || 8080);
-export const HOST = process.env.HOST || '0.0.0.0';
-export const REDIS_HOST = process.env.REDIS_URL || 'cache';
+export const HOST = process.env.HOST || "0.0.0.0";
+export const REDIS_HOST = process.env.REDIS_URL;
 export const REDIS_PORT = Number(process.env.REDIS_PORT || 6379);
-export const JWT_SECRET = process.env.JWT_SECRET || 'shhhhhhhh...this is a super secret';
+export const JWT_SECRET =
+  process.env.JWT_SECRET || "shhhhhhhh...this is a super secret";
 
 // ENV
 export const ENVIRONMENTS = {
-  PRODUCTION: 'production',
-  STAGING: 'staging',
-  DEVELOPMENT: 'development',
-  TEST: 'test',
-}
+  PRODUCTION: "production",
+  STAGING: "staging",
+  DEVELOPMENT: "development",
+  TEST: "test",
+};
 
-export const ENV = process.env.NODE_ENV || process.env.ENV || 'development'; // development, staging, production, test
+export const ENV = process.env.NODE_ENV || process.env.ENV || "development"; // development, staging, production, test
 
 export const ROLES = {
-  ADMIN: 'admin',
-  USER: 'user',
-}
+  ADMIN: "admin",
+  USER: "user",
+};
